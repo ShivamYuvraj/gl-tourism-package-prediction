28dcd33c549106b172a839e387c5cc89e6457161
ShivamYuvraj <shivamyuvraj1@gmail.com>
2025-10-04 13:21:47 +0000

    add everything

---
+++ tourism_project/tourism_project/model_building/prep.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for converting text data in to numerical representation
+from sklearn.preprocessing import LabelEncoder
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi
+DATASET_PATH = 'hf://datasets/Shivam174/gltourism-prediction-data/tourism.csv'
+df = pd.read_csv(DATASET_PATH)
+
+df = df.drop(columns=['CustomerID'])
+df = df.dropna()
+
+# Split into X (features) and y (target)
+X = df.drop(columns=[target_col])
+y = df[target_col]
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+repo_id = 'Shivam174/gltourism-prediction-data'
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id=repo_id,
+        repo_type="dataset",
+    )
